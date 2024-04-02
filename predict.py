08b5fd538756c551598677046818c0a46c13187d
ZhouBaoxian <64571074+ZhouBaoxian@users.noreply.github.com>
2024-04-02 20:55:38 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name: BTEF
+File Name: test.py
+Author: ZhouBaoxian
+Create Date: 2024/3/26
+Description：
+-------------------------------------------------
+"""
 import glob
 import numpy as np
 import torch
